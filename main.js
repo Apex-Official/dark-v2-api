f2e0f83588ea77b3022496864bcc2c7dbb6e5701
RADIO/dev <m29520492@gmail.com>
2025-12-20 12:07:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+import express from "express";
 import { routeLoader } from "./src/lib/routesLoader.js";
 import { apiDocs } from "./src/lib/apiDocs.js";
 
-async function setupApp(app, options = {}) {
+const app = express();
 
+async function setupApp(app) {
   await routeLoader.loadRoutes(app, "api", "/api/v1");
 
-  app.get("/api/v1", apiDocs("/api/v1"));
+  app.get("/api/v1/:section?", apiDocs("/api/v1"));
+
+  app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 }
 
-export default setupApp;
+setupApp(app);
