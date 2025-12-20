12ba48ead9a1f04282eb6aa15ee22bb9f9af8e95
RADIO/dev <m29520492@gmail.com>
2025-12-20 11:11:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { routeLoader } from "./src/lib/routesLoader.js";
+import { apiDocs } from "./src/lib/apiDocs.js";
+
 async function setupApp(app, options = {}) {
 
   await routeLoader.loadRoutes(app, "api", "/api/v1");
 
+  app.get("/api/v1", apiDocs(app, "/api/v1"));
 }
 
-export default setupApp
+export default setupApp;
