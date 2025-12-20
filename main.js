78236c80f29d49cee0777586c77b199721876403
RADIO/dev <m29520492@gmail.com>
2025-12-20 12:05:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ import { apiDocs } from "./src/lib/apiDocs.js";
 
 async function setupApp(app, options = {}) {
 
-  // تحميل APIs
   await routeLoader.loadRoutes(app, "api", "/api/v1");
 
-  // Docs (من غير app)
   app.get("/api/v1", apiDocs("/api/v1"));
 }
 
